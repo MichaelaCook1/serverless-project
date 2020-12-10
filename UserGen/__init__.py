740b04325591cb3c05732dcf3833edcaeba2b9f8
MichaelaCook1 <MCook1@qa.com>
2020-12-10 16:07:56 +0000

    finished app

--- UserGen/__init__.py
+++ UserGen/__init__.py
@@ -1,24 +1,31 @@
 import logging
-
+import requests
 import azure.functions as func
+from azure.cosmos import CosmosClient, PartitionKey, exceptions
 
+endpoint = "https://mcook.documents.azure.com:443/"
+key = "szscnN7yKEo6i67VVrUs88pkByKTV2Etiy9Q6QbGjRhHTQHPC9Pg3CQ39oAQyitxNwfcQAwv2QwcjcV59mcuaw=="
+client = CosmosClient(endpoint, key)
+
+database_name = "username"
+database=client.create_database_if_not_exists(id=username)
+
+container_name = "User"
+container = database.create_container_if_not_exists(id=username, partition_key=PartitionKey(path="/username"), offer_throughput=400)
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    letter= requests.get(https://serverlessproject-mcook.azurewebsites.net/api/LetterGen?code=yjdqO2mQsl8DMIbtLWNvXhtRF6UnVQYinYl1RuuX1rWa/PgCSzjDxQ==).text
+    number= requests.get(https://serverlessproject-mcook.azurewebsites.net/api/NumberGen?code=eTIScavYqUaxlbkidXJFHvqo6lm2iqzteVgUQASTpJt9ujj/Aa0M6g==).text
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    username = ""
+    for i in range(5):
+    username += letter[i]
+    username += number[i]     
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    username_to_add = {
+        "id": username
+    }
+    container.create_item(body=username_to_add)
+
+    return username  
